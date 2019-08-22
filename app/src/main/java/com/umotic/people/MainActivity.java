b38c28261a7ef791050ebc32527452ed588ad395
umoticdeveloper@gmail.com <Troccoly1>
2019-08-22 18:50:17 +0200

    - Saverio Bounce button (Animation)

--- app/src/main/java/com/umotic/people/MainActivity.java
+++ app/src/main/java/com/umotic/people/MainActivity.java
@@ -3,6 +3,10 @@ package com.umotic.people;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void didTapButton(View view) {
+        Button bounceButtonSearch = (Button)findViewById(R.id.bounceButtonSearch);
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+
+        // Use bounce interpolator with amplitude 0.2 and frequency 20
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0, 100);
+        myAnim.setInterpolator(interpolator);
+
+        bounceButtonSearch.startAnimation(myAnim);
+
     }
 }
---
+++ app/src/main/java/com/umotic/people/MyBounceInterpolator.java
@@ -0,0 +1,15 @@
+package com.umotic.people;
+
+public class MyBounceInterpolator implements android.view.animation.Interpolator {
+    private double mAmplitude = 1;
+    private double mFrequency = 10;
+
+    MyBounceInterpolator(double amplitude, double frequency) {
+        mAmplitude = amplitude;
+        mFrequency = frequency;
+    }
+
+    public float getInterpolation(float time) {
+        return (float) Math.sin(mFrequency * time) + 1;
+    }
+}
